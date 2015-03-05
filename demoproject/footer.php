7ae6e77eae953d5f5116738b63ff4e82c4ec5503
ROSE CASS <rose.cass12@gmail.com>
2015-03-05 10:20:38 -0800

    Adding some php magic, cutting things up and calling them as php templates.

---
+++ demoproject/footer.php
@@ -0,0 +1,35 @@
+  <div class="footer">
+    <div class="row footer-links">
+
+      <a href="#" class="large-6 columns"><img src="imgs/footer_logo.png" alt="" /></a>
+
+          <div class="large-2 columns">
+            <h3><a href="#">Contact Us</a></h3>
+            <p><a href="#">Number here</a></p>
+            <p><a href="#"><i class="fa fa-phone"></i>111-111-111</a></p>
+          </div>
+          <div class="large-2 columns">
+            <h3><a href="#">Our Projects</a></h3>
+            <p><a href="#">Lorem Ipsum</a></p>
+            <p><a href="#">More text</a></p>
+            <p><a href="#">Look More Text</a></p>
+          </div>
+            <div class="large-2 columns">
+            <h3><a href="#">Get Social</a></h3>
+            <p><a href="#">Facebook</a></p>
+            <p><a href="#">Twitter</a></p>
+          </div>
+
+    </div>
+    <div class="row footer-copyright">
+        <p><span class="copyright">© 2015 Ipsum projects Inc.</span>
+        <a href="#" class="copyright-link">Privacy</a> | <a href="#" class="copyright-link">Terms</a> | <a href="#" class="copyright-link">Sitemap </a></p>
+    </div>
+  </div>
+  <!-- SECTION FOOTER END-->
+
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/foundation/js/foundation.min.js"></script>
+    <script src="js/app.js"></script>
+  </body>
+</html>
---
+++ demoproject/index.php
@@ -0,0 +1,8 @@
+<?php //  HEAD/NAV  // ?>
+<?php include('header.php'); ?>
+
+<?php // CONTENT // ?>
+<?php include('single.php');?>
+
+<?php // FOOTER // ?>
+<?php include('footer.php'); ?>
