336a2c362c20da6029bbcb831e93ae0482781740
ROSE CASS <rose.cass12@gmail.com>
2015-03-05 13:36:32 -0800

    Minor performance changes.  Compressed multiple font calls and compressed images.

--- demoproject/header.php
+++ demoproject/header.php
@@ -3,32 +3,25 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Foundation</title>
-    <!--<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"> -->
+    <title>DRIVE DIGITAL DEMO</title>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
-    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,400italic' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,400italic|Muli|Montserrat' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="stylesheets/app.css" />
     <script src="bower_components/modernizr/modernizr.js"></script>
   </head>
   <body>
-  
     <div class="row">
       <div class="large-12 columns">
-
         <div class="contain-to-grid fixed">
           <nav class="top-bar" data-topbar role="navigation">
             <ul class="title-area">
               <li class="logo">
                 <a href="http://foundation.zurb.com/docs"><img src="imgs/nav_logo.png"></a>
               </li>
-               <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
               <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
             </ul>
-
             <section class="top-bar-section">
-              <!-- Right Nav Section -->
               <ul class="right">
                 <li class=""><a href="#">Projects</a></li>
                 <li class=""><a href="#">Ipsum</a></li>
@@ -37,10 +30,8 @@
                 <li class=""><a href="#"><i class="fa fa-search"></i></a></li>
                 <li class="has-form"><a href="#" class="medium-7 columns button prime">Button One</a></li>
               </ul>
-
             </section>
           </nav>
         </div>
-
       </div>
     </div>
